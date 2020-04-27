9242e4a4130c48f7cec4f75e745d51b0250dbd3f
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2020-04-27 17:58:53 +0900

    5.0

---
+++ src/socketController.js
@@ -0,0 +1,14 @@
+import events from "./events";
+
+const socketController = socket => {
+  const broadcast = (event, data) => socket.broadcast.emit(event, data);
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+    broadcast(events.newUser, { nickname });
+  });
+  socket.on(events.disconnect, () => {
+    broadcast(events.disconnected, { nickname: socket.nickname });
+  });
+};
+
+export default socketController;
