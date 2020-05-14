76134952cf7d0a18f5bc2fb7eae47b7e0c5025c7
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2020-05-14 10:41:10 +0900

    6.2

--- src/socketController.js
+++ src/socketController.js
@@ -1,17 +1,39 @@
 import events from "./events";
 
-const socketController = socket => {
+let sockets = [];
+
+const socketController = (socket, io) => {
   const broadcast = (event, data) => socket.broadcast.emit(event, data);
+  const superBroadcast = (event, data) => io.emit(event, data);
+  const sendPlayerUpdate = () =>
+    superBroadcast(events.playerUpdate, { sockets });
+
   socket.on("setNickname", ({ nickname }) => {
     socket.nickname = nickname;
+    socket.push({ id: socket.id, point: 0, nickname: nickname });
     broadcast(events.newUser, { nickname });
+    sendPlayerUpdate();
   });
   socket.on(events.disconnect, () => {
+    sockets = socket.filter(aSocket => aSocket.id !== socket.id);
     broadcast(events.disconnected, { nickname: socket.nickname });
+    sendPlayerUpdate();
   });
 
   socket.on(events.sendMsg, ({ message }) => {
     broadcast(event.newMsg, { message, nickname: socket.nickname });
+  });
+
+  socket.on(events.beginPath, ({ x, y }) =>
+    broadcast(events.beganPath, { x, y })
+  );
+
+  socket.on(events.strokePath, ({ x, y, color }) =>
+    broadcast(events.strokedPath, { x, y, color })
+  );
+
+  socket.on(events.fill, ({ color }) => {
+    broadcast(events.filled, { color });
   });
 };
 
